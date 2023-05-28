9ddff0f725b3ad22270c75bc397ff75aaf0e4dfb
RANJAN PALAI <palairanjan2002@gmail.com>
2023-05-28 19:21:09 +0530

    Edyoda Coding Assesment

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+
+import './App.css'
+import { Container } from './components/Container'
+import { Navbar } from './components/Navbar'
+
+function App() {
+ 
+
+  return (
+    <>
+      <Navbar/>
+      <Container/>
+    </>
+  )
+}
+
+export default App
